49da37b794a7e742fdffc9309eedec1519733a06
Alhassan Abdulkadir <farinjini@users.noreply.github.com>
2022-11-05 14:34:16 +0100

    Lowercase URLS Lessons followed from - https://learn.microsoft.com/en-us/aspnet/core/tutorials/razor-pages/?view=aspnetcore-6.0

--- Web.RazorPages01/Program.cs
+++ Web.RazorPages01/Program.cs
@@ -3,6 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+    options.LowercaseQueryStrings = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
